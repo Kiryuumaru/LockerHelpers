18953ba86b6738a761fc347a40e7f37b11ebc357
Clynt Neiko Rupinta <neiko@meldcx.com>
2024-06-28 08:04:02 +0800

    Updated CICD

---
+++ build/Build.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Linq;
+using Nuke.Common;
+using Nuke.Common.CI;
+using Nuke.Common.Execution;
+using Nuke.Common.IO;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tooling;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Utilities.Collections;
+using NukeBuildHelpers;
+using NukeBuildHelpers.Common.Attributes;
+using NukeBuildHelpers.Entry;
+using NukeBuildHelpers.Entry.Extensions;
+using NukeBuildHelpers.Runner.Abstraction;
+using NukeBuildHelpers.RunContext.Extensions;
+using NukeBuildHelpers.Common.Enums;
+
+class Build : BaseNukeBuildHelpers
+{
+    public static int Main() => Execute<Build>(x => x.Version);
+
+    public override string[] EnvironmentBranches { get; } = ["prerelease", "master"];
+
+    public override string MainEnvironmentBranch { get; } = "master";
+
+    [SecretVariable("NUGET_AUTH_TOKEN")]
+    readonly string? NuGetAuthToken;
+
+    [SecretVariable("GITHUB_TOKEN")]
+    readonly string? GithubToken;
+
+    BuildEntry LockerHelpersBuild => _ => _
+        .AppId("locker_helpers")
+        .RunnerOS(RunnerOS.Ubuntu2204)
+        .Condition(true)
+        .Execute(context =>
+        {
+            string version = "0.0.0";
+            string? releaseNotes = null;
+            if (context.TryGetBumpContext(out var bumpContext))
+            {
+                version = bumpContext.AppVersion.Version.ToString();
+                releaseNotes = bumpContext.AppVersion.ReleaseNotes;
+            }
+            else if (context.TryGetPullRequestContext(out var pullRequestContext))
+            {
+                version = pullRequestContext.AppVersion.Version.ToString();
+            }
+            OutputDirectory.DeleteDirectory();
+            DotNetTasks.DotNetClean(_ => _
+                .SetProject(RootDirectory / "LockerHelpers" / "LockerHelpers.csproj"));
+            DotNetTasks.DotNetBuild(_ => _
+                .SetProjectFile(RootDirectory / "LockerHelpers" / "LockerHelpers.csproj")
+                .SetConfiguration("Release"));
+            DotNetTasks.DotNetPack(_ => _
+                .SetProject(RootDirectory / "LockerHelpers" / "LockerHelpers.csproj")
+                .SetConfiguration("Release")
+                .SetNoRestore(true)
+                .SetNoBuild(true)
+                .SetIncludeSymbols(true)
+                .SetSymbolPackageFormat("snupkg")
+                .SetVersion(version)
+                .SetPackageReleaseNotes(releaseNotes)
+                .SetOutputDirectory(OutputDirectory));
+        });
+
+    PublishEntry LockerHelpersPublish => _ => _
+        .AppId("locker_helpers")
+        .RunnerOS(RunnerOS.Ubuntu2204)
+        .Execute(context =>
+        {
+            if (context.RunType == RunType.Bump)
+            {
+                DotNetTasks.DotNetNuGetPush(_ => _
+                    .SetSource("https://nuget.pkg.github.com/kiryuumaru/index.json")
+                    .SetApiKey(GithubToken)
+                    .SetTargetPath(OutputDirectory / "**"));
+                DotNetTasks.DotNetNuGetPush(_ => _
+                    .SetSource("https://api.nuget.org/v3/index.json")
+                    .SetApiKey(NuGetAuthToken)
+                    .SetTargetPath(OutputDirectory / "**"));
+            }
+        });
+}
